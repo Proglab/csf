d2b1f61aba4ea14de545dd1f51877f4ae324c407
Fabrice <info@proglab.com>
2020-07-11 03:05:21 +0200

    :sparkles: User Fixtures

---
+++ src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UsersFixtures extends Fixture
+{
+    private $userPasswordEncoder;
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        foreach($this->getDatas() as $data)
+        {
+            $user = new User();
+            $user->setFirstname($data['firstname']);
+            $user->setLastname($data['lastname']);
+            $user->setEmail($data['email']);
+            $user->setPassword($this->userPasswordEncoder->encodePassword(
+                $user,
+                $data['password']
+            ));
+            $user->setRoles($data['roles']);
+            $user->setIsVerified($data['isVerified']);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDatas()
+    {
+        return [
+            [
+                'firstname' => 'Admin',
+                'lastname' => 'istrator',
+                'email' => 'info@proglab.com',
+                'password' => 'admin',
+                'roles' => ['ROLE_ADMIN'],
+                'isVerified' => 1,
+            ],
+            [
+                'firstname' => 'basic',
+                'lastname' => 'user',
+                'email' => 'manu@absolute-fx.com',
+                'password' => 'user',
+                'roles' => ['ROLE_USER'],
+                'isVerified' => 1,
+            ],
+        ];
+    }
+}
