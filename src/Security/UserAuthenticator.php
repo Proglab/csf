1858e468c8cd09ff230e5d75b275571a25ce5636
Fabrice <info@proglab.com>
2020-07-11 03:04:52 +0200

    :bug: redirect after login

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -96,8 +96,7 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
     protected function getLoginUrl()
